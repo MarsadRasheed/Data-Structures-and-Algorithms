4c01ccc23478c64fbfeecfda1a16ae00509e2cd1
Marsad Rasheed <54879111+MarsadRasheed@users.noreply.github.com>
2019-12-06 23:31:22 +0500

    update double linked list
    insertion at head and print function and comments added

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+// node class
+
 class node
 {
 public:
@@ -9,27 +11,71 @@ public:
 	int data;
 };
 
+// double linked list class
+
 class DoubleLinkedList {
 private:
 	node* head;
 	node* tail;
 public:
+	// constructor for initializing values
 	DoubleLinkedList() {
 		head = NULL;
 		tail = NULL;
 	}
+	
+	// function prototypes for inserting 
+	void insertionAtHead(int num);  	// insertion at head 
+	void insertionAtTail(int num);		// insertion at tail
+	void insertAT(int position, int num);	// insertion at particular
+	void insertSort(int num);		// insertion on sorting based. node will be added in sorting order
 
-	void insertionAtHead(int num);
-	void printList();
-	void insertionAtTail(int num);
-	void insertAT(int position, int num);
-	int sizeOfList();
-	int deletionAtHead();
-	int deletionAtTail();
-	int deleteAt(int position);
-	int deleteBy(int num);
-	void reverseList();
-	void sortList();
-	void insertSort(int num);
+	//function prototypes for deleting
+	
+	int deletionAtHead(); 		// deletion at head
+	int deletionAtTail(); 		// deletion at tail
+	int deleteAt(int position);	// delete at particular position
+	int deleteBy(int num);		// delete by value 
+	
+	void reverseList(); 	// reverse list function prototype 
+	void sortList();	// sort list function prototype
+	void printList();       // print list function prototype
+	int sizeOfList();       // size of list getter function
 
 };
+
+// function to insertion at head
+
+void DoubleLinkedList::insertionAtHead(int num) {
+	node* temp = new node;
+	temp->data = num;
+	temp->next = NULL;
+	temp->previous = NULL;
+
+	if (head == NULL) {
+		head = temp;
+		tail = temp;
+	}
+	else {
+		temp->next = head;
+		head->previous = temp;
+		temp->previous = NULL;
+		head = temp;
+	}
+}
+
+// function to print double linked list
+
+void DoubleLinkedList::printList() {
+	node* temp = head;
+	if ( head == NULL){
+		return;
+	}else{
+		while ( temp!= NULL){
+			cout << temp->data << "   ";
+			temp = temp->next;
+		}
+		cout << endl;
+	}
+}
+
